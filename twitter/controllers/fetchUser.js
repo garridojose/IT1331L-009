4d385d967723f561c365c39d9397af531acb540c
Jose Garrido <141913305+garridojose@users.noreply.github.com>
2023-09-14 17:51:32 +0800

    Add files via upload

---
+++ twitter/controllers/fetchUser.js
@@ -0,0 +1,11 @@
+const userDetailsConfig = {
+    "api": {
+        "uri": ': `https://twitter154.p.rapidapi.com/user/details?username=',
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Key":"7788b4bbc9msh3006c4522df34d1p1499fbjsn97c659546827",
+            "X-RapidAPI-Host": "twitter154.p.rapidapi.com"
+        }
+    }
+
+}
